b18c76d81b7c3e53e1901630a1627295ef8f2ea5
cuKiis-o01 <2301315008fj@cyber-u.ac.jp>
2025-01-29 01:50:06 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,8 +1,7 @@
-var express = require('express'); 
-var router = express. Router();
+var express = require('express');
+var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
-
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
@@ -13,16 +12,15 @@ const client = new MongoClient(uri);
 router.use(cors());
 
 router.get('/', async (req, res) => {
-// データベース、 コレクションを指定
-const database = client.db('notes'); 
+// データベース、コレクションを指定
+const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが1のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
 
-res.json(note.title);
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
+
+res.json(note);
 })
 
 module.exports = router;
-
